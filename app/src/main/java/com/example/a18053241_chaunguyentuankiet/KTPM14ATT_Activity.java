7d01e72051ef125074950d944d58559220ed03c7
kietkiet2332000 <kietkiet2332000@gmail.com>
2021-09-16 11:04:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a18053241_chaunguyentuankiet/KTPM14ATT_Activity.java
@@ -0,0 +1,14 @@
+package com.example.a18053241_chaunguyentuankiet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KTPM14ATT_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_k_t_p_m14_a_t_t_);
+    }
+}
