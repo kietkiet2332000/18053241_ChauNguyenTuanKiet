0f247263094de199e2239c0ae5f806dda933b8d5
kietkiet2332000 <kietkiet2332000@gmail.com>
2021-09-21 13:47:53 +0700

    s

---
+++ app/src/main/java/com/example/a18053241_chaunguyentuankiet/ACtivity2.java
@@ -0,0 +1,14 @@
+package com.example.a18053241_chaunguyentuankiet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ACtivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_ctivity2);
+    }
+}
